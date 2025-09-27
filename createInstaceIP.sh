aeb7be15d0d97c924a6f8ad01145871a6d403cd1
sowjanya <sowji.muppala@gmail.com>
2025-09-27 10:53:35 +0530

    add

--- createInstaceIP.sh
+++ createInstaceIP.sh
@@ -3,7 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0696dc92e81135eee"
 
-for instance in @$
+for instance in $@
 do
     Instance_ID=$(aws ec2 run-instances  --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 

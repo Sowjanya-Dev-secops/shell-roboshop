bd33d8d1a3d16680a3aa0f108980762e922fb2aa
sowjanya <sowji.muppala@gmail.com>
2025-09-27 15:48:43 +0530

    add

--- mongodb.sh
+++ mongodb.sh
@@ -38,7 +38,7 @@ VALIDATE $? "enable mongodb"
 systemctl start mongod
 VALIDATE $? "start mongodb"
 
-sed -i 's/127.0.0.1/0.0.0.0/g /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "allowing remote connections to mongodb"
 
 systemctl restart mongod

f0ac126cc1c8dde059e9c793c91990b6140819cc
sowjanya <sowji.muppala@gmail.com>
2025-09-27 21:55:45 +0530

    add

--- catalouge.set.sh
+++ catalouge.set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-trap 'echo "there is an error in $LINENO , Command is $BASH_COMMAND" 'ERR
+trap 'echo "there is an error in $LINENO , Command is $BASH_COMMAND"' ERR
 
 R="\e[31m"
 G="\e[32m"
